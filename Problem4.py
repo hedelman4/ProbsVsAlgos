2f42c67adab5f432a69ee563dcc14dbfec89ac03
Harry <hte488@gmail.com>
2021-11-08 13:00:51 -0500

    Completed problem 4

---
+++ Problem4.py
@@ -0,0 +1,23 @@
+def sort_012(input_list):
+    index = 0
+    for _ in range(len(input_list)):
+        if input_list[index] == 0:
+            input_list.insert(0,input_list.pop(index))
+            index += 1
+        elif input_list[index] == 2:
+            input_list.append(input_list.pop(index))
+        else:
+            index += 1
+    return input_list
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
